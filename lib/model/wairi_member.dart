420b23edb8186904df7685e75011482778650833
jeonghyun77 <heelli0801@naver.com>
2023-10-23 17:04:32 +0900

    wairimember

---
+++ lib/model/wairi_member.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+class WairiMember {
+  int idx;
+  int type;
+  int level;
+  int status;
+  String id;
+  String email;
+  String phone;
+  String name;
+  String nickname;
+  String passwd;
+  int regdate;
+  int lastUpdate;
+  String exitdate;
+  int exit_admin_idx;
+  String last_visit;
+  String social_type;
+  String social_naver;
+  String social_kakao;
+  String social_google;
+  String social_facebook;
+  int is_website;
+  int channelType;
+  String channelTypeText;
+  String channelLink;
+  String ci;
+  String di;
+  String gender;
+  int birth;
+  int lastSignin;
+  int refererRoot;
+  String refererRootInput;
+  int isTest;
+  int agreeMsg;
+}
